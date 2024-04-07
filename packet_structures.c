1e1ae16bc38619de8bd90be6b16004fc4afbdcc9
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-04-07 13:30:47 +0200

    added packet_structures.c impl of init functions

---
+++ packet_structures.c
@@ -0,0 +1,62 @@
+#include "packet_structures.h"
+#include <arpa/inet.h>
+#include <endian.h>
+
+#define CONN_ID 1
+#define CONACC_ID 2
+#define CONRJT_ID 3
+#define DATA_ID 4
+#define ACC_ID 5
+#define RJT_ID 6
+#define RCVD_ID 7
+
+void init_CONN(CONN *conn, uint64_t session_id, uint8_t protocol_id, 
+                uint64_t nbr_of_bytes)
+{
+    conn->package_type_id = CONN_ID;
+    conn->session_id = htobe64(session_id);
+    conn->protocol_id = protocol_id;
+    conn->len_of_bytes_seq = htobe64(nbr_of_bytes);
+}
+
+void init_CONACC(CONACC *conacc, uint64_t session_id)
+{
+    conacc->package_type_id = CONACC_ID;
+    conacc->session_id = htobe64(session_id);
+}
+
+void init_CONRJT(CONRJT *conrjt, uint64_t session_id)
+{
+    conrjt->package_type_id = CONRJT_ID;
+    conrjt->session_id = htobe64(session_id);
+}
+
+void init_DATA(DATA *data, uint64_t session_id, uint64_t package_id, 
+                uint32_t nbr_of_bytes, char *bytes_to_send)
+{
+    data->package_type_id = DATA_ID;
+    data->session_id = htobe64(session_id);
+    data->package_id = htobe64(package_id);
+    data->nbr_of_bytes_to_be_sent = htobe32(nbr_of_bytes);
+    data->seq_of_bytes = bytes_to_send;
+}
+
+void init_ACC(ACC *acc, uint64_t session_id, uint64_t package_id)
+{
+    acc->package_type_id = ACC_ID;
+    acc->session_id = htobe64(session_id);
+    acc->package_id = htobe64(package_id);
+}
+
+void init_RJT(RJT *rjt, uint64_t session_id, uint64_t package_id)
+{
+    rjt->package_type_id = RJT_ID;
+    rjt->session_id = htobe64(session_id);
+    rjt->package_id = htobe64(package_id);
+}
+
+void init_RCVD(RCVD *rcvd, uint64_t session_id)
+{
+    rcvd->package_type_id = RCVD_ID;
+    rcvd->session_id = htobe64(session_id);
+}
