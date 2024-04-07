70ae2c0fd31c4de06147e1df523a29a6d583a315
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-04-07 12:00:05 +0200

    added protconst.h file

---
+++ protconst.h
@@ -0,0 +1,11 @@
+#ifndef PROTCONST_H
+#define PROTCONST_H
+
+// MAX_WAIT constant defines how many seconds server waits for sent package
+#define MAX_WAIT 10
+
+// If after MAX_WAIT seconds confirmation about sent package hasn't come from
+//  server we send this package again
+#define MAX_RETRANSMITS 5
+
+#endif
