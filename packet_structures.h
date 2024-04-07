742d0832a1b7ba02bcc426357985a2d9a915fc23
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-04-07 12:16:03 +0200

    started implementing packet_structures.h

--- packet_structures.h
+++ packet_structures.h
@@ -1,5 +1,23 @@
 #ifndef PACKET_STRUCTURES_H
 #define PACKET_STRUCTURES_H
 
+#include <inttypes.h>
+#include <stddef.h>
+#include <sys/types.h>
+
+typedef struct __attribute__((__packed__))
+{
+    uint8_t package_type_id;
+    uint64_t session_id;
+    uint8_t protocol_id;
+    uint64_t len_of_bytes_seq;
+} CONN;
+
+typedef struct __attribute__((__packed__))
+{
+    uint8_t package_type_id;
+    uint64_t session_id;
+} CONACC;
+
 
 #endif
