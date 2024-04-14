27230ccf2cf031db4f41d9f551d04c848ada7686
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-04-14 16:59:58 +0200

    added read_data_to_buffer func in server_UDP_lib since it will also be needed when receiving DATA not only when receiving CONN

--- server_UDP_lib.c
+++ server_UDP_lib.c
@@ -19,6 +19,37 @@
 #define DEFAULT_FLAG 0
 #define SUCCESS 0
 
+// - Function reads maximally BUFFOR_SIZE bytes to buff
+// - Before reading function zeros buffer
+// - If recvfrom read <= 0 bytes func returns -1, otherwise nbr of bytes read
+// - Function sets variables *client_address and client_addr_len
+ssize_t read_data_to_buffer(int socket_fd, char *buff,
+                            struct sockaddr_in *client_address,
+                            socklen_t *client_address_len)
+{
+    memset(buff, 0, sizeof(buff));
+
+    // UDP gets data as datagrams that are stored in queue, so after we do
+    // recvfrom, we read whole datagram from queue, so if we dont have
+    // enough space in buffor part of data is lost. Thus first we will read
+    // whole datagram into buffer, then cast it on our structures i.e. CONN.
+    ssize_t read_bytes = recvfrom(socket_fd, buff, BUFFOR_SIZE,
+                                  DEFAULT_FLAG,
+                                  (struct sockaddr *)client_address,
+                                  (socklen_t *)client_address_len);
+    if (read_bytes < 0)
+    {
+        make_error_msg(__FUNCTION__, " - read_bytes < 0");
+        return -1;
+    }
+    if (read_bytes == 0)
+    {
+        make_error_msg(__FUNCTION__, " - read_bytes == 0");
+        return -1;
+    }
+    return read_bytes;
+}
+
 int check_if_CONN(char *buff, ssize_t read_bytes, CONN *conn)
 {
     if (cast_buff_to(conn, sizeof(*conn), buff, (size_t)read_bytes) != SUCCESS)
@@ -42,7 +73,7 @@ int check_if_CONN(char *buff, ssize_t read_bytes, CONN *conn)
 }
 
 int send_CONRJT(int socket_fd, struct sockaddr_in *client_address,
-                 socklen_t client_address_len, uint64_t session_id)
+                socklen_t client_address_len, uint64_t session_id)
 {
     CONRJT conrjt;
 
@@ -65,6 +96,39 @@ int send_CONRJT(int socket_fd, struct sockaddr_in *client_address,
     return 0;
 }
 
+int send_CONACC(int socket_fd, struct sockaddr_in *client_address,
+                socklen_t client_address_len, uint64_t session_id)
+{
+
+    CONACC conacc;
+
+    init_CONACC(&conacc, session_id);
+
+    ssize_t sent_length = sendto(socket_fd, &conacc, sizeof(conacc),
+                                 DEFAULT_FLAG,
+                                 (struct sockaddr *)client_address,
+                                 client_address_len);
+    if (sent_length < 0)
+    {
+        make_error_msg(__FUNCTION__, " - sent len < 0");
+        return -1;
+    }
+    else if (sent_length != sizeof(conacc))
+    {
+        make_error_msg(__FUNCTION__, " - sent_len not equal to size of data we wanted to send");
+        return -1;
+    }
+    return 0;
+}
+
+void UDP_data_receive_handler(int socket_fd, )
+{
+}
+
+void UPDR_data_receive_handler()
+{
+}
+
 void UDP_server_handler(int socket_fd, struct sockaddr_in *server_address)
 {
     printf("UDPserver is listening on port %" PRIu16 "\n",
@@ -74,32 +138,26 @@ void UDP_server_handler(int socket_fd, struct sockaddr_in *server_address)
 
     while (true)
     {
-        memset(buff, 0, sizeof(buff));
-
         struct sockaddr_in client_address;
         socklen_t client_address_len = (socklen_t)sizeof(client_address);
-
-        // UDP gets data as datagrams that are stored in queue, so after we do
-        // recvfrom, we read whole datagram from queue, so if we dont have
-        // enough space in buffor part of data is lost. Thus first we will read
-        // whole datagram into buffer, then cast it on our structures i.e. CONN.
-        ssize_t read_bytes = recvfrom(socket_fd, buff, BUFFOR_SIZE,
-                                      DEFAULT_FLAG,
-                                      (struct sockaddr *)&client_address,
-                                      (socklen_t *)&client_address_len);
-        if (read_bytes < 0)
-        {
-            make_error_msg(__FUNCTION__, " - read_bytes < 0");
+        ssize_t read_bytes = read_data_to_buffer(socket_fd, buff,
+                                                 &client_address, 
+                                                 &client_address_len);
+        if (read_bytes <= 0)
             continue;
-        }
 
         CONN conn;
 
         if (check_if_CONN(buff, read_bytes, &conn) != SUCCESS)
         {
-            // send CONNRJT
             send_CONRJT(socket_fd, &client_address, client_address_len,
                         conn.session_id);
+            continue;
+        }
+
+        if (send_CONACC(socket_fd, &client_address, client_address_len,
+                        conn.session_id) != SUCCESS)
+        {
             continue;
         }
 
